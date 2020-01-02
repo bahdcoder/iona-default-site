6878b351195b069fc87399d71eb6dc1b91f0ce43
Kati Frantz <bahdcoder@gmail.com>
2020-01-02 14:30:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const fs = require('fs')
 const http = require('http')
+const { networkInterfaces } = require('os')
 
 const port = process.env.PORT || 23457
-
 
 const page = fs.readFileSync('index.html')
 
 
 const server = http.createServer((request, response) => {
+  const getLocalExternalIp = () => [].concat.apply([], Object.values(networkInterfaces()))
+    .filter(details => details.family === 'IPv4' && !details.internal)
+    .pop().address
+
+  console.log(getLocalExternalIp)
+
   return response.end(page)
 })
 
